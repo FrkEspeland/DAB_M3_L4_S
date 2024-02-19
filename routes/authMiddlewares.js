deb3675ba033c29fd642bdd649247811debadd03
FrkEspeland <146803041+FrkEspeland@users.noreply.github.com>
2024-02-19 10:31:11 +0100

    Auth Commit

---
+++ routes/authMiddlewares.js
@@ -0,0 +1,12 @@
+
+//checkIfAuthorized() function
+module.exports = {
+    checkIfAuthorized: function(req, res, next) {
+        if(req.user == null) {
+            res.status(401).send(new Error());
+            return;
+        }
+        if(req.user.role == "Admin" || req.user.role == "User")
+            next();
+    }
+}
